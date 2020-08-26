54004b039b5c1d7a439a2d98df30378720f69085
mansi-done <61977666+mansi-done@users.noreply.github.com>
2020-08-26 12:02:53 +0530

    Update settings.py

--- calcu/settings.py
+++ calcu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')k1x%gl1+&j*2a+c0k!q89rv=7*xejmpa-e_^&ck2oqb_!96om'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
